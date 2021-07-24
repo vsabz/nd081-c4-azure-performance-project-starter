2cd3b770cb8351a49893026a2989af343fad6954
Valeh Sabziyev <valeh.sabziyev@gmail.com>
2021-07-25 00:42:56 +0200

    logger added

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -104,10 +104,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
+            logger.info('cat-vote', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
+            logger.info('dog-vote', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
